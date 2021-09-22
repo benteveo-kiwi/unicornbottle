54ae00782cc2d66687fc8186723938be8d25e769
Sam Roque-Worcel <sam@worcel.com>
2021-09-21 17:27:12 -0700

    New models structure. Add Fuzzer insertion point model.

---
+++ database_models.py
@@ -0,0 +1,272 @@
+from mitmproxy.net.http.http1 import assemble
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey, UniqueConstraint, Boolean
+from sqlalchemy import or_, not_, and_
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, RelationshipProperty, Query
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+from typing import Dict, Optional, Any, Union, TypeVar, Type, List, Tuple
+from unicornbottle.serializers import Request, Response, ExceptionSerializer, DatabaseWriteItem
+
+RR = TypeVar('RR', bound='RequestResponse')
+Base : Any = declarative_base()
+
+class InvalidScopeName(Exception):
+    pass
+
+class Scope(Base):
+    """
+    This table contains basic metadata regarding scope.
+    """
+    __tablename__ = "scope"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, index=True)
+
+    urls : RelationshipProperty = relationship("ScopeURL") 
+
+class ScopeURL(Base):
+    """
+    This table stores scope information for specific URLs. Only URLs explicitly
+    included are crawled, and a login_script can optionally be used for each URL.
+
+    PostgreSQL wildcards, like the ones used by the LIKE statement, are allowed
+    on the pretty_url_like column. The "login_script" column refers to login
+    scripts as used and defined by the crawler.
+
+    The negative column determines whether this scope url is meant to be
+    exclusionary, i.e. whether we should exclude URLs that match the
+    pretty_url_like.
+    """
+    __tablename__ = "scope_url"
+
+    id = Column(Integer, primary_key=True)
+    scope_id = Column(Integer, ForeignKey('scope.id'), nullable=False, index=True)
+    pretty_url_like = Column(String, nullable=False, index=True)
+    login_script = Column(String)
+    negative = Column(Boolean, default=False)
+
+class EndpointMetadata(Base):
+    """
+    This table contains metadata related to particular endpoints. Endpoints are
+    defined as (`pretty_url`,`method`) pairs. This may encompass several
+    `RequestResponses` if more than one request has ever been recorded for an
+    endpoint.
+    """
+    __tablename__ = "endpoint_metadata"
+    __table_args__ = (UniqueConstraint('pretty_url', 'method', name='_url_method_uc'),)
+
+    id = Column(Integer, primary_key=True)
+    pretty_url = Column(String, index=True)
+    method = Column(String, index=True)
+
+    fuzz_count = Column(Integer, default=0, nullable=False)
+
+    crawl_count = Column(Integer, default=0, nullable=False) # Successful crawl count.
+
+    # Crawl failed due to bad HTTP status code or bad URL in general.  The idea
+    # is that these kinds of failures may occur even if there are no bugs in
+    # our code. E.g. a broken link may trigger this error.
+    crawl_fail_count = Column(Integer, default=0, nullable=False) 
+
+    # Crawl failed due to unhandled exception.  These generally should be
+    # indicative of a failure on our end to either handle a specific scenario,
+    # and should not occur in normal operations. E.g. An error with the
+    # login_script may trigger this flag.
+    crawl_exception_count = Column(Integer, default=0, nullable=False) 
+
+    request_responses : RelationshipProperty = relationship("RequestResponse") 
+
+    def __repr__(self) -> str:
+        return "%s (%s) fuzz_count:%s crawl_count:%s crawl_fail_count:%s crawl_exception_count:%s" % (self.pretty_url,
+                self.method, self.fuzz_count, self.crawl_count, self.crawl_fail_count, self.crawl_exception_count)
+
+    @staticmethod
+    def get_endpoints_by_scope(db:Session, scope_name:str, limit:int,
+            max_crawl_count:int, method:Optional[str]=None,
+            order_by:Optional[Column]=None) -> Query:
+        """
+        Returns the query object required in order to get all endpoints filtered by scope.
+
+        Args:
+            db: the db as returned by `unicornbottle.database.database_connect`
+            scope_name: the scope as stored in the `Scope.name` model.
+            limit: Absolute maximum number of results to return.
+            max_crawl_count: exclude rows with a `crawl_count` higher than this value.
+            method: filter by method if present.
+            order_by: order by. If not present, will sort by crawl_count asc.
+        """
+        try:
+            scope = db.query(Scope).filter(Scope.name == scope_name).one()
+        except NoResultFound:
+            raise InvalidScopeName("A scope named %s does not exist in the schema" % scope_name)
+
+        # Join.
+        join_filter = (EndpointMetadata.pretty_url.like(ScopeURL.pretty_url_like) & # type: ignore
+                (ScopeURL.scope_id == scope.id) & (ScopeURL.login_script != None)) 
+
+        rows:Query = db.query(EndpointMetadata, ScopeURL)\
+                .join(ScopeURL, join_filter, isouter=True)
+
+        # Filter.
+        url_filters = []
+        for scope_url in scope.urls:
+            pretty_url_like = EndpointMetadata.pretty_url.like(scope_url.pretty_url_like)
+            if scope_url.negative:
+                url_filters.append(not_(pretty_url_like))
+            else:
+                url_filters.append(pretty_url_like)
+
+        # Optional filters based on function parameters.
+        if len(url_filters) > 0:
+            rows = rows.filter(and_(*url_filters))
+
+        if max_crawl_count != -1:
+            rows = rows.filter(EndpointMetadata.crawl_count <= max_crawl_count)
+
+        if method:
+            rows = rows.filter(EndpointMetadata.method == method)
+
+        # Order and Limit.
+        if order_by:
+            rows = rows.order_by(order_by)
+        else:
+            rows = rows.order_by(EndpointMetadata.crawl_count.asc())
+
+        if limit != -1:
+            rows = rows.limit(limit)
+
+        return rows
+
+    @staticmethod
+    def get_crawl_endpoints(db:Session, scope_name:str, limit:int, max_crawl_count:int) -> List[Tuple[str, Optional[str]]]:
+        """
+        Gets endpoints that will be sent to the RabbitMQ queue as crawl tasks.
+
+        Args:
+            db: the db as returned by `unicornbottle.database.database_connect`
+            scope_name: the scope as stored in the `Scope.name` model.
+            limit: Absolute maximum number of results to return.
+            max_crawl_count: exclude rows with a `crawl_count` higher than this value.
+        """
+        rows = EndpointMetadata.get_endpoints_by_scope(db, scope_name, limit, max_crawl_count)
+
+        # Transform and increment crawl_count.
+        urls = []
+        for row in rows.all():
+            endpoint_metadata = row[0]
+            scope_url = row[1]
+
+            endpoint_metadata.crawl_count = endpoint_metadata.crawl_count + 1
+            urls.append((endpoint_metadata.pretty_url, scope_url.login_script))
+
+        return urls
+
+    @staticmethod
+    def discovered_endpoints(db:Session, scope_name:str) -> List:
+        """
+        Obtains endpoints discovered.
+
+        Args:
+            db: the db as returned by `unicornbottle.database.database_connect`
+            scope_name: the scope as stored in the `Scope.name` model.
+        """
+        rows = EndpointMetadata.get_endpoints_by_scope(db, scope_name,
+                limit=-1, max_crawl_count=-1, method=None,
+                order_by=EndpointMetadata.pretty_url)
+
+        endpoints = []
+        for row in rows.all():
+            endpoint_metadata = row[0]
+            endpoints.append(endpoint_metadata)
+
+        return endpoints
+
+class RequestResponse(Base):
+    """
+    This table contains the requests sent through the proxy and, if there are
+    any, also information regarding either the response or the error. These map
+    almost 1:1 to attributes that are present in the `mitmproxy` API
+    documentation, so if you need more information regarding any of the fields
+    you can also find information there.
+
+    The following columns are not part of that API:
+
+    - fuzz_count: number of times we have fuzzed this endpoint.
+    - crawl_count: the number of times we have initiated a crawl from this URL.
+      Our crawling strategy is recursive and time-bound so we prioritize
+      starting from endpoints we have not yet scanned from where we can.
+
+    Please note that a different schema is used for each "target", in order to
+    avoid all requests ever sent through the proxy from being stored in a
+    single database table and make performance tuning a little simpler.
+
+    @see https://docs.mitmproxy.org/dev/api/mitmproxy/http.html#Request
+    @see https://docs.mitmproxy.org/dev/api/mitmproxy/http.html#Response
+    """
+    __tablename__ = "request_response"
+
+    id = Column(Integer, primary_key=True)
+    metadata_id = Column(Integer, ForeignKey('endpoint_metadata.id'), nullable=False, index=True)
+    pretty_url = Column(String, index=True)
+    pretty_host = Column(String, index=True)
+    path = Column(String, index=True)
+    scheme = Column(String)
+    port = Column(Integer)
+    method = Column(String)
+    response_status_code = Column(Integer)
+    exception = Column(JSON)
+    request = Column(JSON)
+    response = Column(JSON)
+
+    @classmethod
+    def createFromDWI(cls, dwi:DatabaseWriteItem) -> RR:
+        """
+        Helper method for creating a `RequestResponse` object from a `models.DatabaseWriteItem`
+
+        Args:
+            dwi: Input `DatabaseWriteItem`.
+        """
+        exc = None
+        if dwi.exception is not None:
+            exc = dwi.exception.toJSON()
+
+        resp = None
+        resp_status_code=None
+        if dwi.response is not None:
+            resp = Response(dwi.response.get_state()).toJSON()
+            resp_status_code=dwi.response.status_code
+
+        req = Request(dwi.request.get_state()).toJSON()
+            
+        return cls(pretty_url=dwi.request.pretty_url,
+                pretty_host=dwi.request.pretty_host, path=dwi.request.path,
+                scheme=dwi.request.scheme, port=dwi.request.port,
+                method=dwi.request.method,
+                response_status_code=resp_status_code,
+                exception=exc,
+                request=req,
+                response=resp)
+
+    def to_plain(self) -> str:
+        """
+        Convert this database entry to a plaintext representation of request
+        response. A plain text representation in this context means the
+        plaintext of the request concatenated to the plaintext of the response.
+        """
+
+        if not self.request or not self.response:
+            return "[-] Could not generate plaintext representation of request_response."
+
+        request = Request.fromJSON(str(self.request)).toMITM()
+        response = Response.fromJSON(str(self.response)).toMITM()
+
+        request.decode(strict=False)
+        response.decode(strict=False)
+
+        req_string = assemble.assemble_request(request).decode('utf-8', errors='ignore')
+        resp_string = assemble.assemble_response(response).decode('utf-8', errors='ignore')
+
+        return str(req_string + "\n\n" + resp_string)
+
+
---
+++ serializers.py
@@ -0,0 +1,208 @@
+from enum import Enum
+from typing import Dict, Optional, Any, Union, TypeVar, Type, List, Tuple
+import base64
+import json
+import mitmproxy.net.http
+
+MS = TypeVar('MS', bound='MessageSerializer')
+
+class RequestEncoder(json.JSONEncoder):
+    """
+    Performs encoding of byte arrays as base64. The rationale for doing this is
+    to prevent having to deal with every known encoding known in the universe
+    and instead transmit bytes as they are.
+
+    Byte arrays that are encoded are prefixed by "application/base64:" in order
+    to facilitate detection of base64 by the decoder.
+    """
+    def default(self, obj : Any) -> Any:
+        if isinstance(obj, (bytes, bytearray)):
+            encoded = base64.b64encode(obj).decode("ascii")
+            return "application/base64:" + encoded
+
+        return json.JSONEncoder.default(self, obj)
+
+class RequestDecoder(json.JSONDecoder):
+    """
+    Decodes requests encoded by RequestEncoder.
+
+    Recursively iterates through all objects and decodes strings if they match
+    the required prefix.
+    """
+    def __init__(self, *args, **kwargs) -> None: #type:ignore
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def decode_base64(self, string : str) -> Union[str, bytes]:
+        """
+        Decodes strings and returns the corresponding bytes if they are base64
+        
+        Args:
+            string: the input string decode or leave as is.
+        """
+        if string.startswith("application/base64:"):
+            splat = string.split(":")
+            return base64.b64decode(splat[1])
+        else:
+            return string
+
+    def object_hook(self, data : Any) -> Any:
+        if isinstance(data, (dict, list)):
+            for k, v in (data.items() if isinstance(data, dict) else enumerate(data)):
+                if isinstance(v, str):
+                    data[k] = self.decode_base64(v)
+
+                self.object_hook(v)
+
+        return data
+
+class MessageSerializer():
+    def __init__(self, state : dict) -> None:
+        """
+        Internal representation of request/response objects for the proxy and
+        server instances. Can be used to transmit mitmproxy's internal
+        representations.
+
+        Args:
+            state: request state as exported by the
+                mitmproxy.Request.get_state() method.
+        """
+
+        self.state = state
+
+    def toJSON(self) -> str:
+        """
+        Converts the object into a JSON string. Byte arrays are encoded into
+        base64.
+        """
+
+        data = self.state
+        return json.dumps(data, cls=RequestEncoder)
+
+    @classmethod
+    def fromJSON(cls : Type[MS], json_str : Union[bytes, str]) -> MS:
+        """
+        Creates a Request object from a JSON string.
+
+        Raises:
+            json.decoder.JSONDecodeError: if you give it bad JSON.
+        """
+        j = json.loads(json_str)
+        state = json.loads(json_str, cls=RequestDecoder)
+        return cls(state)
+
+class Request(MessageSerializer):
+
+    def toMITM(self) -> mitmproxy.net.http.Request:
+        """
+        Grabs data stored in the request state and converts it into a
+        mitmproxy.http.Request object.
+        """
+        return mitmproxy.net.http.Request(**self.state)
+
+class Response(MessageSerializer):
+    def toMITM(self) -> mitmproxy.net.http.Response:
+        """
+        Grabs data stored in the request state and converts it into a
+        mitmproxy.http.Response object.
+        """
+        return mitmproxy.net.http.Response(**self.state)
+
+class ExceptionSerializer(MessageSerializer):
+    """
+    Stores information about an exception.
+    """
+
+    def __init__(self, type:str, value:str, tb:str):
+        self.state : Dict[str, str] = {}
+
+        self.state["type"] = type
+        self.state["value"] = value
+        self.state["tb"] = tb
+
+class DatabaseWriteItem():
+    """
+    Simple data structure for communication between the `send_request` thread
+    and the `thread_postgres` thread.
+    """
+    def __init__(self, target_guid:str, request:mitmproxy.net.http.Request,
+            response:Optional[mitmproxy.net.http.Response], exception:Optional[ExceptionSerializer]) -> None:
+        """
+        Default constructor.
+
+        Args:
+            target_guid: the guid sent in the X-UB-GUID header.
+            request: the `Request` associated with this entry.
+            response: the response. May be null if an error occurred that
+                prevented the retrieval of the response, such as a timeout.
+            exception: the exception associated with this failure. May be None if
+                there were no errors.
+        """
+        self.target_guid = target_guid
+        self.request = request
+        self.response = response
+        self.exception = exception
+
+class InvalidFuzzLocation(Exception):
+    pass
+
+class FuzzParamType(Enum):
+    PARAM_URL = 1
+    PARAM_BODY = 2
+    PARAM_MULTIPART = 3
+    PARAM_JSON = 4
+    HEADER = 5
+
+class FuzzLocation():
+    """
+    This class represents a location within a HTTP Request where we will be
+    inserting payloads. 
+    
+    It inherits from Request because the orignal request information is stored
+    within it and is used for the generation of modified HTTP requests.
+    """
+
+    def __init__(self, state:dict, param_type:FuzzParamType, param_name:bytes):
+        """
+        Main constructor.
+
+        Args:
+            state: request state as exported by the
+                mitmproxy.Request.get_state() method.
+            param_type: fuzz parameter type. see `FuzzParamType`.
+            param_name: parameter name.
+        """
+        self.base_request_state = state
+
+        self.param_type = param_type
+        self.param_name = param_name
+
+    def fuzz(self, value:bytes):
+        """
+        Inserts the value at the insertion point.
+
+        Args:
+            value: the value to insert into the request.
+        """
+        request = mitmproxy.net.http.Request(**dict(self.base_request_state))
+
+        if self.param_type == FuzzParamType.PARAM_URL:
+            request.query[self.param_name] = value
+        elif self.param_type == FuzzParamType.PARAM_BODY:
+            if request.urlencoded_form:
+                request.urlencoded_form[self.param_name] = value
+            else:
+                raise InvalidFuzzLocation("Can't insert into urlencoded form.")
+        elif self.param_type == FuzzParamType.PARAM_MULTIPART:
+            if request.multipart_form:
+                request.multipart_form[self.param_name.encode('utf-8')] = value
+            else:
+                raise InvalidFuzzLocation("Can't insert into multipart form.")
+        elif self.param_type == FuzzParamType.PARAM_JSON:
+            body_json = json.loads(request.content)
+            body_json[self.param_name] = value
+            request.text = json.dumps(body_json)
+        elif self.param_type == FuzzParamType.HEADER:
+            request.headers[self.param_name] = value
+
+        return request
+
